afc030d74b78567d84e643f5af9ce8a1e1fa10aa
Alan Escobedo <ingescobedoalan4@gmail.com>
2022-11-29 20:29:43 -0600

    add eslint + standard

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <h1>Hello</h1>
   </React.StrictMode>
-);
+)
 
-serviceWorkerRegistration.register();
-reportWebVitals();
+serviceWorkerRegistration.register()
+reportWebVitals()
